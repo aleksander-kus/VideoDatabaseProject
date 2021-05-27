e5f54a6b19a02e8ac85293edb85d05aa12947f96
Aleksander Kuś <01151536@pw.edu.pl>
2021-05-27 15:20:09 +0200

    changed order of columns in select 1

--- select.sql
+++ select.sql
@@ -1,7 +1,7 @@
 USE PD2
 
 -- 1. Time watched by each user on each channel
-SELECT Nick, Name as 'Channel Name', SUM(DATEDIFF(ms, '00:00:00', Duration)) / 1000 as 'Seconds Watched'
+SELECT Name as 'Channel Name', Nick, SUM(DATEDIFF(ms, '00:00:00', Duration)) / 1000 as 'Seconds Watched'
 FROM Users u
 Join Watch_History wh on wh.UserID = u.UserID
 Join Videos v on v.VideoID = wh.VideoID
