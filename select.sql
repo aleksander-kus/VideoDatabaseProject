367dff2fc55b51686f5e2d58335fd3efa9d2b2b3
Aleksander Kuś <01151536@pw.edu.pl>
2021-05-27 12:11:15 +0200

    implemented querry 1

--- select.sql
+++ select.sql
@@ -1,7 +1,11 @@
 USE PD2
---SUM(DATEDIFF(ms, '00:00:00', Duration))
-SELECT Nick, Email, Watch_Time, Title, Duration, Name
+
+-- 1. Time watched by each user on each channel
+SELECT Nick, Name as 'Channel Name', SUM(DATEDIFF(ms, '00:00:00', Duration)) / 1000 as 'Seconds Watched'
 FROM Users u
 Join Watch_History wh on wh.UserID = u.UserID
 Join Videos v on v.VideoID = wh.VideoID
 Join Channels c on c.ChannelID = v.ChannelID
+GROUP BY Nick, c.Name, c.ChannelID
+ORDER BY c.ChannelID;
+
